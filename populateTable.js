91b4885707e7a74fb43187a90d344ecc35a24718
PRenkel <60927909+PRenkel@users.noreply.github.com>
2022-06-02 17:11:10 +0200

    chore: stuff

--- populateTable.js
+++ populateTable.js
@@ -3,29 +3,32 @@ fetchAndDisplay(allCharactersUrl);
 
 
 async function fetchAndDisplay(url) {
-    fetch(url).then(res => {
-        res.json().then(jsonData => {
+    try {
+        fetch(url).then(res => {
+            res.json().then(jsonData => {
 
-            let table = document.getElementById("table");
+                let table = document.getElementById("table");
 
-            jsonData.forEach(entry => {
-                let tableRow = table.insertRow();
+                jsonData.forEach(entry => {
+                    let tableRow = table.insertRow();
 
-                let nameRow = tableRow.insertCell(0);
-                nameRow.innerHTML = entry.characterName;
+                    let nameRow = tableRow.insertCell(0);
+                    nameRow.innerHTML = entry.characterName;
 
-                let actorNameRow = tableRow.insertCell(1);
-                actorNameRow.innerHTML = entry.actorName;
+                    let actorNameRow = tableRow.insertCell(1);
+                    actorNameRow.innerHTML = entry.actorName;
 
-                let siblingRow = tableRow.insertCell(2);
-                siblingRow.innerHTML = entry.siblings?.join(', ') || 'No siblings';
+                    let siblingRow = tableRow.insertCell(2);
+                    siblingRow.innerHTML = entry.siblings?.join(', ') || 'No siblings';
 
-                let killedRow = tableRow.insertCell(3);
-                killedRow.innerHTML = entry.killed?.join(', ') || 'No kills'
+                    let killedRow = tableRow.insertCell(3);
+                    killedRow.innerHTML = entry.killed?.join(', ') || 'No kills'
+                })
             })
-        })
-            .catch(err => {
-                window.alert(err.message);
-            });
-    });
+        });
+    } catch (e) {
+        window.alert(e.message)
+    }
 }
+
+module.exports = fetchAndDisplay;
--- populateTable.test.js
+++ populateTable.test.js
@@ -1,15 +1,82 @@
-const populateTable = require('./populateTable');
+const fetchAndDisplay = require('./populateTable');
 const allCharactersUrl = "http://localhost:3000/characters";
+const oneCharacterUrl = "http://localhost:3000/character/";
+const mockCharacterData = [
+    {
+        "id": "1",
+        "characterName": "Arya Stark",
+        "houseName": "Stark",
+        "characterImageThumb": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk5MTYwNDc0OF5BMl5BanBnXkFtZTcwOTg2NDg1Nw@@._V1._SX100_SY140_.jpg",
+        "characterImageFull": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk5MTYwNDc0OF5BMl5BanBnXkFtZTcwOTg2NDg1Nw@@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
+        "characterLink": "/character/ch0158604/",
+        "actorName": "Maisie Williams",
+        "actorLink": "/name/nm3586035/",
+        "siblings": [
+            "Robb Stark",
+            "Sansa Stark",
+            "Bran Stark",
+            "Rickon Stark"
+        ],
+        "killed": [
+            "Red Keep Stableboy",
+            "Frey Soldier #1",
+            "Polliver",
+            "Rorge",
+            "Ghita",
+            "Meryn Trant",
+            "The Waif",
+            "Black Walder Rivers",
+            "Lothar Frey",
+            "Walder Frey",
+            "Petyr Baelish",
+            "The Night King",
+            "White Walker",
+            "Viserion"
+        ],
+        "parents": [
+            "Eddard Stark",
+            "Catelyn Stark"
+        ],
+        "guardedBy": [
+            "Nymeria"
+        ]
+    },
+    {
+        "id": "2",
+        "characterName": "Brienne of Tarth",
+        "characterImageThumb": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYzNDY4NzgzOV5BMl5BanBnXkFtZTcwNDM5ODg4OQ@@._V1._SX100_SY140_.jpg",
+        "characterImageFull": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYzNDY4NzgzOV5BMl5BanBnXkFtZTcwNDM5ODg4OQ@@._V1_.jpg",
+        "characterLink": "/character/ch0254503/",
+        "actorName": "Gwendoline Christie",
+        "actorLink": "/name/nm3729225/",
+        "kingsguard": true,
+        "killed": [
+            "Soldier Tom",
+            "Stannis Baratheon",
+            "Bolton Officer"
+        ],
+        "serves": [
+            "Renly Baratheon",
+            "Catelyn Stark",
+            "Sansa Stark",
+            "Bran Stark"
+        ],
+        "servedBy": [
+            "Podrick Payne"
+        ]
+    }];
 
 test('fetches data', async () => {
     let receivedMessage = null;
+
     Object.defineProperty(global, "window", {
         value: {
             alert: jest.fn()
         }
     });
-    jest.spyOn(window, 'alert').mockImplementation(message => receivedMessage = message);
 
+
+    jest.spyOn(window, 'alert').mockImplementation(message => receivedMessage = message);
     await fetchAndDisplay(allCharactersUrl);
     expect(window.alert).toHaveBeenCalledTimes(1);
 })
