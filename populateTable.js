48c836d6c127266fa49cc38259e855e75e17a2af
PRenkel <60927909+PRenkel@users.noreply.github.com>
2022-06-20 15:40:48 +0200

    feat: linting

--- populateTable.js
+++ populateTable.js
@@ -1,31 +1,30 @@
-const allCharactersUrl = "http://localhost:3000/characters";
+const allCharactersUrl = 'http://localhost:3000/characters'
 
-async function fetchAndDisplay(url = allCharactersUrl) {
-    try {
-        await fetch(url).then(res => {
-            res.json().then(jsonData => {
+async function fetchAndDisplay (url = allCharactersUrl) {
+  try {
+    await fetch(url).then(res => {
+      res.json().then(jsonData => {
+        const table = document.getElementById('table')
+        jsonData.forEach(entry => {
+          const tableRow = table.insertRow()
 
-                let table = document.getElementById("table");
-                jsonData.forEach(entry => {
-                    let tableRow = table.insertRow();
+          const nameRow = tableRow.insertCell(0)
+          nameRow.innerHTML = entry.characterName
 
-                    let nameRow = tableRow.insertCell(0);
-                    nameRow.innerHTML = entry.characterName;
+          const actorNameRow = tableRow.insertCell(1)
+          actorNameRow.innerHTML = entry.actorName
 
-                    let actorNameRow = tableRow.insertCell(1);
-                    actorNameRow.innerHTML = entry.actorName;
+          const siblingRow = tableRow.insertCell(2)
+          siblingRow.innerHTML = entry.siblings?.join(', ') || 'No siblings'
 
-                    let siblingRow = tableRow.insertCell(2);
-                    siblingRow.innerHTML = entry.siblings?.join(', ') || 'No siblings';
-
-                    let killedRow = tableRow.insertCell(3);
-                    killedRow.innerHTML = entry.killed?.join(', ') || 'No kills'
-                })
-            })
-        });
-    } catch (e) {
-        window.alert(e.message)
-    }
+          const killedRow = tableRow.insertCell(3)
+          killedRow.innerHTML = entry.killed?.join(', ') || 'No kills'
+        })
+      })
+    })
+  } catch (e) {
+    window.alert(e.message)
+  }
 }
 
-module.exports = fetchAndDisplay;
+module.exports = fetchAndDisplay
