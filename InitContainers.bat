39f628f491a8861cfcce71165469b0732d4c2f8a
PRenkel <60927909+PRenkel@users.noreply.github.com>
2022-06-20 19:01:24 +0200

    feat: initContainers.bat

---
+++ InitContainers.bat
@@ -0,0 +1,4 @@
+docker pull annawithtwon/ci-cd-semester-project
+docker run -d -p 3000:3000 --name backend annawithtwon/ci-cd-semester-project
+docker pull renkelp/ci-group-n-2022-frontend
+docker run -d -p 80:8080 --name frontend renkelp/ci-group-n-2022-frontend
